64f3eb6e131704b574597e2a47de4b66915cf536
Scott Cowe <scott.t.cowe@gmail.com>
2024-04-07 12:28:16 +0000

    Set wezterm font to firacode

--- modules/wezterm/wezterm.lua
+++ modules/wezterm/wezterm.lua
@@ -3,5 +3,5 @@ local wezterm = require "wezterm";
 return {
   enable_wayland = false,
   color_scheme = "Catppuccin Mocha",
-  --font = wezterm.font "Fira Code", 
+  font = wezterm.font "FiraCode Nerd Font", 
 }
