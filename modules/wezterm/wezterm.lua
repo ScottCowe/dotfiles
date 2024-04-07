e8aa03d0e0d65326c99469cb75c0b7d752eb8d89
Scott Cowe <scott.t.cowe@gmail.com>
2024-04-07 19:23:45 +0000

    Updated wezterm config

--- modules/wezterm/wezterm.lua
+++ modules/wezterm/wezterm.lua
@@ -1,7 +1,88 @@
-local wezterm = require "wezterm";
+local wezterm = require "wezterm"
 
-return {
-  enable_wayland = false,
-  color_scheme = "Catppuccin Mocha",
-  font = wezterm.font "FiraCode Nerd Font", 
+local config = wezterm.config_builder()
+
+config.enable_wayland = false
+
+config.color_scheme = "Catppuccin Mocha"
+config.font = wezterm.font "FiraCode Nerd Font"
+
+config.use_fancy_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = true
+
+config.disable_default_key_bindings = true
+config.keys = {
+  {
+    key = "Enter",
+    mods = "ALT",
+    action = wezterm.action.SpawnTab "DefaultDomain"
+  },
+  {
+    key = "q",
+    mods = "ALT",
+    action = wezterm.action.CloseCurrentTab { confirm = true }
+  },
+  {
+    key = "q",
+    mods = "ALT|SHIFT",
+    action = wezterm.action.CloseCurrentPane { confirm = true }
+  },
+  {
+    key = "h",
+    mods = "ALT|SHIFT",
+    action = wezterm.action.SplitPane {
+      direction = "Left"
+    }
+  },
+  {
+    key = "j",
+    mods = "ALT|SHIFT",
+    action = wezterm.action.SplitPane {
+      direction = "Down"
+    }
+  },
+  {
+    key = "k",
+    mods = "ALT|SHIFT",
+    action = wezterm.action.SplitPane {
+      direction = "Up"
+    }
+  },
+  {
+    key = "l",
+    mods = "ALT|SHIFT",
+    action = wezterm.action.SplitPane {
+      direction = "Right"
+    }
+  },
+  {
+    key = "h",
+    mods = "ALT",
+    action = wezterm.action.ActivatePaneDirection "Left"
+  },
+  {
+    key = "j",
+    mods = "ALT",
+    action = wezterm.action.ActivatePaneDirection "Down"
+  },
+  {
+    key = "k",
+    mods = "ALT",
+    action = wezterm.action.ActivatePaneDirection "Up"
+  },
+  {
+    key = "l",
+    mods = "ALT",
+    action = wezterm.action.ActivatePaneDirection "Right"
+  },
 }
+
+for i = 1, 9 do
+  table.insert(config.keys, {
+    key = tostring(i),
+    mods = "ALT",
+    action = wezterm.action.ActivateTab(i - 1)
+  })
+end
+
+return config
