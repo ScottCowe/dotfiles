ef88bf49ebc648e30072248a781d874357f37e05
Scott Cowe <scott.t.cowe@gmail.com>
2024-04-27 20:52:46 +0100

    set default branch name to main for all repos

--- modules/git/default.nix
+++ modules/git/default.nix
@@ -10,6 +10,10 @@ in {
       enable = true;
       userName = "Scott Cowe";
       userEmail = "scott.t.cowe@gmail.com";
+
+      extraConfig = {
+        init.defaultBranch = "main";
+      };
     };
   };
 }
