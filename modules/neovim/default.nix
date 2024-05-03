86afe5049b7c310f1a92dd540fda0eca442c94e1
Scott Cowe <scott.t.cowe@gmail.com>
2024-05-03 23:25:14 +0100

    trying to make java lsp do useful stuff

--- modules/neovim/default.nix
+++ modules/neovim/default.nix
@@ -89,12 +89,6 @@ in {
         require("gitsigns").setup()
 
         require("nvim-autopairs").setup({})
-
-        local config = {
-            cmd = { "${pkgs.jdt-language-server}/bin/jdtls" },
-            root_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw'}, { upward = true })[1]),
-        }
-        require('jdtls').start_or_attach(config)
       '';
     };
   };
--- modules/neovim/plugin/lsp.lua
+++ modules/neovim/plugin/lsp.lua
@@ -1,22 +1,21 @@
+local bufmap = function(keys, func, bufnr)
+  vim.keymap.set('n', keys, func, { buffer = bufnr })
+end
+
 local on_attach = function(_, bufnr)
+  bufmap('<leader>r', vim.lsp.buf.rename, bufnr)
+  bufmap('<leader>a', vim.lsp.buf.code_action, bufnr)
 
-  local bufmap = function(keys, func)
-    vim.keymap.set('n', keys, func, { buffer = bufnr })
-  end
+  bufmap('gd', vim.lsp.buf.definition, bufnr)
+  bufmap('gD', vim.lsp.buf.declaration, bufnr)
+  bufmap('gI', vim.lsp.buf.implementation, bufnr)
+  bufmap('<leader>D', vim.lsp.buf.type_definition, bufnr)
 
-  bufmap('<leader>r', vim.lsp.buf.rename)
-  bufmap('<leader>a', vim.lsp.buf.code_action)
+  bufmap('gr', require('telescope.builtin').lsp_references, bufnr)
+  bufmap('<leader>s', require('telescope.builtin').lsp_document_symbols, bufnr)
+  bufmap('<leader>S', require('telescope.builtin').lsp_dynamic_workspace_symbols, bufnr)
 
-  bufmap('gd', vim.lsp.buf.definition)
-  bufmap('gD', vim.lsp.buf.declaration)
-  bufmap('gI', vim.lsp.buf.implementation)
-  bufmap('<leader>D', vim.lsp.buf.type_definition)
-
-  bufmap('gr', require('telescope.builtin').lsp_references)
-  bufmap('<leader>s', require('telescope.builtin').lsp_document_symbols)
-  bufmap('<leader>S', require('telescope.builtin').lsp_dynamic_workspace_symbols)
-
-  bufmap('K', vim.lsp.buf.hover)
+  bufmap('K', vim.lsp.buf.hover, bufnr)
 
   vim.api.nvim_buf_create_user_command(bufnr, 'Format', function()
     vim.lsp.buf.format()
@@ -33,19 +32,37 @@ local lspconfig = require('lspconfig')
 lspconfig.nixd.setup({})
 
 lspconfig.lua_ls.setup({
-    on_attach = on_attach,
-    capabilities = capabilities,
-    settings = {
-        Lua = {
-            workspace = {
-              checkThirdParty = false,
-              library = {
-                vim.env.VIMRUNTIME
-              }
-            },
-            telemetry = { enable = false },
-        },
-    }
+  on_attach = on_attach,
+  capabilities = capabilities,
+  settings = {
+    Lua = {
+      workspace = {
+        checkThirdParty = false,
+        library = {
+          vim.env.VIMRUNTIME
+        }
+      },
+      telemetry = { enable = false },
+    },
+  }
 })
 
+local jdtlsCommand = "nix eval --raw nixpkgs#jdt-language-server.outPath"
+local cmdHandler = io.popen(jdtlsCommand);
+local jdtlsPath = cmdHandler:read("*a")
+cmdHandler:close()
 
+local jdtls = require('jdtls')
+
+jdtls.start_or_attach({
+  cmd = { jdtlsPath .. "/bin/jdtls" },
+  root_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw'}, { upward = true })[1]),
+  on_attach = on_attach,
+  contentProvider = { preferred = 'fernflower' },
+  sources = {
+    organizeImports = {
+      starThreshold = 9999;
+      staticStarThreshold = 9999;
+    }
+  },
+})
