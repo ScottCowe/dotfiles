6d1b25c411b9569819ac5f5a3f4412a3c0c5287e
Scott Cowe <scott.t.cowe@gmail.com>
2024-05-15 01:29:28 +0100

    added themes to prismlauncher (to be improved)

--- modules/packages/default.nix
+++ modules/packages/default.nix
@@ -12,7 +12,6 @@ in {
       cmatrix
       thunderbird
       pavucontrol
-      alsa-oss
       libreoffice
     ]; 
   };
--- modules/prismlauncher/default.nix
+++ modules/prismlauncher/default.nix
@@ -7,7 +7,12 @@ in {
   options.modules.prismlauncher = { enable = mkEnableOption "prismlauncher"; };
   config = mkIf cfg.enable {
     home.packages = with pkgs; [
-      prismlauncher
+      (prismlauncher.override {
+        jdks = [ jdk21 jdk17 jdk8 ];
+        additionalPrograms = [ alsa-oss ];
+      })
     ];
+
+    home.file.".local/share/PrismLauncher/themes/Catppuccin-Mocha".source = ./Catppuccin-Mocha;
   };
 }
