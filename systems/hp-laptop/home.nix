c49b7836a0d279fe3e0548b202f9e3004bf03379
Scott Cowe <scott.t.cowe@gmail.com>
2024-06-10 20:36:28 +0100

    updated laptop config (untested) and cleaned up hardware configs

--- systems/hp-laptop/home.nix
+++ systems/hp-laptop/home.nix
@@ -1,31 +1,46 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   imports = [
-    ../../modules
     ../../home
   ];
 
   config = {
-    neovim.enable = true;
+    gui.enable = true;
+    dev.enable = true;
+    gaming.prismlauncher.enable = true;
 
-    modules = {
-      colors.enable = true;
-      rofi.enable = true;
-      gimp.enable = false;
-      fonts.enable = true;
-      thunar.enable = true;
-      firefox.enable = true;
-      wezterm.enable = true;
-      discord.enable = true;
-      packages.enable = true;
-      hyprland.enable = true;
-      alacritty.enable = true;
-      prismlauncher.enable = false;
-      mako.enable = true;
-      wlogout.enable = true;
+    common = {
+      easyeffects.enable = true;
       lf.enable = true;
-      waybar.enable = true;
+      discord.enable = true;
+      firefox.enable = true;
+      gimp.enable = true;
+      macchina.enable = true;
+      bottom.enable = true;
+      libreoffice.enable = true;
+      thunderbird.enable = true;
+      bat.enable = true;
+      eza.enable = true;
+      quickemu.enable = true;
+    };
+
+    gaming.prismlauncher.theme = pkgs.stdenv.mkDerivation {
+      name = "Catppuccin-Mocha-theme";
+
+      src = pkgs.fetchurl {
+        url = "https://github.com/PrismLauncher/Themes/releases/download/2024-04-01_1711994750/Catppuccin-Mocha-theme.zip";
+        sha256 = "197xvd388ddag1qsv34rikff6hybddzyh26dam08q5hhmq11rbay";
+      };
+
+      dontUnpack = true;
+
+      installPhase = ''
+        mkdir -p $out
+        ${pkgs.unzip}/bin/unzip $src -d $out/
+        mv $out/themes/Catppuccin-Mocha/* $out
+        rm -rf $out/themes/
+      '';
     };
   };
 }
