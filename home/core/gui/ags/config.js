1a757c878a6199c7a5140abb9e42873e801ea9b6
Scott Cowe <scott.t.cowe@gmail.com>
2024-06-18 23:29:11 +0100

    added bar widget

--- home/core/gui/ags/config.js
+++ home/core/gui/ags/config.js
@@ -1,5 +1,15 @@
 import { applauncher } from "./applauncher.js"
+import { Bar } from "./bar.js"
 
 App.config({
     windows: [applauncher],
 })
+
+App.config({
+    style: "./style.css",
+    windows: [
+      Bar(0),
+      Bar(1)
+    ],
+})
+
